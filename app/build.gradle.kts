b87330b897d95ed0245f3a90f84d6f1714d08f0b
nathanael2005 <nathanaeleshetu8901@gmail.com>
2026-07-18 15:49:21 +0300

    Bump version to 1.0.3 to match current tag

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
     applicationId = "com.aistudio.vibro.xyzq"
     minSdk = 24
     targetSdk = 36
-    versionCode = 1
-    versionName = "1.0"
+    versionCode = 3
+    versionName = "1.0.3"
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
   }
